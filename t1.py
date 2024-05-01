8a226b59681cbb32b8432409ee5bba0d4104f848
harshavennapu <107935397+harshavennapu@users.noreply.github.com>
2024-05-01 13:47:06 +0530

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,9 @@
+#creating a list 
+my_list=[1,2,3,4,5]
+#adding an element to the list
+my_list.append(90)
+#removing an element from the list
+my_list.remove(3)
+#modifying an element from the list
+my_list[0]=10
+print("updated list:",my_list)
